8c6576a60223cc4d05f04c23773a84223d967f5d
BoldCodeer <micsrelles@gmail.com>
2021-12-26 20:03:56 +0800

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+    	if(Auth::user()->hasRole('user')) {
+    		return view('userdash');
+    	}
+    	elseif(Auth::user()->hasRole('reseller')) {
+    		return view('resellerdash');
+    	}
+    	elseif(Auth::user()->hasRole('admin')) {
+    		return view('dashboard');
+    	}
+    }
+    public function myprofile()
+    {
+    	return view('users.profile');
+    }
+}
